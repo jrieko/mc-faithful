9107044a0488b6971bed715ae0814939b1c6636e
Jenny Taylor <jen.rieko@gmail.com>
2017-11-23 16:02:21 -0700

    scripts to create symlinks for individual resource packs per feature

---
+++ mklinks.bat
@@ -0,0 +1,7 @@
+pushd %~dp0
+set this_dir=%CD%
+popd
+
+cd %APPDATA%\.minecraft\resourcepacks
+mklink /D mcf-observer "%this_dir%\observer"
+mklink /D mcf-sticky-piston "%this_dir%\sticky-piston"
---
+++ mklinks.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+this_dir="$(dirname $(readlink -f $0))"
+
+cd ~/.minecraft/resourcepacks
+ln -s "$this_dir/observer" mcf-observer 
+ln -s "$this_dir/sticky-piston" mcf-sticky-piston
